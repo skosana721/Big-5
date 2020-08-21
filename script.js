636bb2b45662de929e8fcaf803ab44423e1db4a7
Masekolg <masekolg@gmail.com>
2020-08-21 13:57:13 +0200

    5 big five project

---
+++ script.js
@@ -0,0 +1,36 @@
+const setNewImage =()=>{
+document.getElementById("lion").src = "https://cdn.pixabay.com/photo/2016/01/15/08/55/lion-1141303_960_720.jpg";
+
+
+
+}
+const setOldImage =() =>{
+    document.getElementById("lion").src = "https://cdn.pixabay.com/photo/2019/08/11/17/25/lion-4399437_960_720.jpg";
+  
+}
+const setNewLeopard =()=>{
+    
+    document.getElementById("leopard").src = "https://cdn.pixabay.com/photo/2014/11/03/17/40/leopard-515510_960_720.jpg";
+}
+const setOldLeopard =()=>{
+    document.getElementById("leopard").src = "https://cdn.pixabay.com/photo/2017/08/03/19/42/leopard-2578114_960_720.jpg";
+
+}
+const setNewRhino=()=>{
+    document.getElementById("rhino").src = "https://cdn.pixabay.com/photo/2013/08/11/19/47/rhino-171654_960_720.jpg";
+}
+const setOldRhino=()=>{
+    document.getElementById("rhino").src = "https://cdn.pixabay.com/photo/2013/02/27/14/50/africa-86621_960_720.jpg";
+}
+const setNewBuffalo=()=>{
+    document.getElementById("buffalo").src = "https://cdn.pixabay.com/photo/2016/06/15/13/28/water-buffalo-1458907_960_720.jpg";
+}
+const setOldBuffalo=()=>{
+    document.getElementById("buffalo").src = "https://cdn.pixabay.com/photo/2016/09/20/08/51/buffalo-1682061_960_720.jpg";
+}
+const setNewElephant=()=>{
+    document.getElementById("elephant").src = "https://cdn.pixabay.com/photo/2020/01/24/22/32/elephant-4791438_960_720.jpg";
+}
+const setOldElephant=()=>{
+    document.getElementById("elephant").src = "https://cdn.pixabay.com/photo/2017/12/21/04/54/elephant-3031112_960_720.jpg";
+}
